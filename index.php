ccb8426be04314cbf1e8ce80ee88853828cfc8eb
Diamond705 <n.diamond.705@gmail.com>
2017-07-31 23:31:22 +0100

    page layout set for calculator

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+
+    <title>Basic Calculator</title>
+</head>
+
+<body>
+    <div class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+<h2>Basic Javascript Calculator</h2>
+        </div>
+    </div>
+
+    <div id="ww">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+                    <!-- basic calculator -->
+                    
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+    </div><!-- /ww -->
+    
+    	<div id="footer">
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-lg-6">
+					<h4>My Links</h4>
+					<p>
+						<a href="#">Medium</a><br/>
+						<a href="#">Twiter</a><br/>
+						<a href="#">Github</a>
+					</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-6">
+					<h4>Description</h4>
+					<p>Simple calculator made functional by Javascript logic.</p>
+				</div><!-- /col-lg-4 -->
+			
+			</div>
+		
+		</div>
+	</div>
+</body>
+
+</html>
