dde69cfbb354bd6e91ff74c8c0c14eeaa0b17220
Uche Ndububa <n.diamond.705@gmail.com>
2017-08-04 00:25:45 +0100

    added calculator inputs with basic JS testing

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="">
+	<link rel="shortcut icon" href="">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/main.css">
 
     <title>Basic Calculator</title>
 </head>
@@ -24,38 +26,41 @@
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2 centered">
 					<!-- basic calculator -->
+				<div id="bc">
 					<form name="calc">
 						<input type="text" name="output" placeholder="0" value="" ><br>
 
-						<input type="button" name="" value=" 1 ">
-						<input type="button" name="" value=" 2 ">
-						<input type="button" name="" value=" 3 ">
-						<input type="button" name="" value=" / "><br>
+						<input class="zoom green" type="button" name="" value=" 1 " onclick="document.calc.output.value+='1'" >
+						<input class="zoom green" type="button" name="" value=" 2 " onclick="document.calc.output.value+='2'" >
+						<input class="zoom green" type="button" name="" value=" 3 " onclick="document.calc.output.value+='3'" >
+						<input class="zoom green" type="button" name="" value=" / " onclick="document.calc.output.value+='/'" ><br>
 
-						<input type="button" name="" value=" 4 ">
-						<input type="button" name="" value=" 5 ">
-						<input type="button" name="" value=" 6 ">
-						<input type="button" name="" value=" X "><br>
+						<input class="zoom green" type="button" name="" value=" 4 " onclick="document.calc.output.value+='4'" >
+						<input class="zoom green" type="button" name="" value=" 5 " onclick="document.calc.output.value+='5'" >
+						<input class="zoom green" type="button" name="" value=" 6 " onclick="document.calc.output.value+='6'" >
+						<input class="zoom green" type="button" name="" value=" X " onclick="document.calc.output.value+='*'" ><br>
 
-						<input type="button" name="" value=" 7 ">
-						<input type="button" name="" value=" 8 ">
-						<input type="button" name="" value=" 9 ">
-						<input type="button" name="" value=" - "><br>
+						<input class="zoom green" type="button" name="" value=" 7 " onclick="document.calc.output.value+='7'" >
+						<input class="zoom green" type="button" name="" value=" 8 " onclick="document.calc.output.value+='8'" >
+						<input class="zoom green" type="button" name="" value=" 9 " onclick="document.calc.output.value+='9'" >
+						<input class="zoom green" type="button" name="" value=" - " onclick="document.calc.output.value+='-'" ><br>
 
-						<input type="button" name="" value=" ( ">
-						<input type="button" name="" value=" ) ">
-						<input type="button" name="" value=" 0 ">
-						<input type="button" name="" value=" + "><br>
+						<input class="zoom green" type="button" name="" value=" ( " onclick="document.calc.output.value+='('" >
+						<input class="zoom green" type="button" name="" value=" ) " onclick="document.calc.output.value+=')'" >
+						<input class="zoom green" type="button" name="" value=" 0 " onclick="document.calc.output.value+='0'" >
+						<input class="zoom green" type="button" name="" value=" + " onclick="document.calc.output.value+='+'" ><br>
 
-						<input type="button" name="" value=" c ">
-						<input type="button" name="" value=" . ">
-						<input type="button" name="" value=" = ">
-						<input type="button" name="" value=" % "><br>
+						<input class="zoom green" type="button" name="" value=" c " onclick="document.calc.output.value=''" >
+						<input class="zoom green" type="button" name="" value=" . " onclick="document.calc.output.value+='.'" >
+						<input class="zoom green" type="button" name="" value=" = " onclick="document.calc.output.value=eval(document.calc.output.value)" >
+						<input class="zoom green" type="button" name="" value=" % " onclick="document.calc.output.value+='%'" ><br>
 						
 						
 						
 						
 					</form>
+
+				</div>
                     
 				</div><!-- /col-lg-8 -->
 			</div><!-- /row -->
@@ -84,6 +89,11 @@
 		
 		</div>
 	</div>
+
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+	<script type="text/javascript" src="assets/js/hover.zoom.conf.js"></script>
+	<script type="text/javascript" src="assets/js/hover.zoom.js"></script>
 </body>
 
 </html>
