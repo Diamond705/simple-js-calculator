3641496fe7798bd5ee5e94451bab9625d8da8b16
Uche Ndububa <n.diamond.705@gmail.com>
2017-08-04 00:58:23 +0100

    updated js logic comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
+// Function to calculate any inputed value
 function calculate(){
     document.calc.output.value = eval(document.calc.output.value);
 }
 
+// Function to clear input field
 function erase() {
     document.calc.output.value = '';
 }
 
+//  Function to replicate any number/character inputed
 function entries(input) {
     document.calc.output.value+=input;
 }
